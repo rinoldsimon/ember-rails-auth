8ddf833e321ee0d0b3881a87f35a9a55a925a930
Rinold Simon <crisrinold@gmail.com>
2016-03-29 08:32:25 +0530

    edit action for posts-comments

--- embercli/app/routes/posts-comments/edit.js
+++ embercli/app/routes/posts-comments/edit.js
@@ -5,6 +5,12 @@ export default Ember.Route.extend({
     this.render('posts-comments.edit', { into: 'application' });
   },
   actions: {
+  	save: function() {
+      //this.controller.get('model').save().then(() => {
+      this.currentModel.save().then(() => {
+        this.transitionTo('posts.show', this.modelFor('posts-comments'));
+      });
+    },
     cancel: function() {
       this.transitionTo('posts.show', this.modelFor('posts-comments'));
     }
