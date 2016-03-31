50f11688a8b77ad45b439a105e79704c858ff534
Rinold Simon <crisrinold@gmail.com>
2016-03-31 16:50:26 +0530

    initial materialize design

--- embercli/app/routes/posts/show.js
+++ embercli/app/routes/posts/show.js
@@ -3,8 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     deleteComment: function(comment) {
-      comment.deleteRecord();
-      comment.save();
+      if(confirm('Are you sure?')) {
+      	//comment.destroyRecord();
+      	comment.deleteRecord();
+        comment.save();
+      }
     }
   }
 });
