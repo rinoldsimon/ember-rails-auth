62d67b7fb09613a2ed49e542f626def32f4f7a45
Rinold Simon <crisrinold@gmail.com>
2016-04-11 00:25:49 +0530

    generate authenticated_token in devise

--- railsapi/config/routes.rb
+++ railsapi/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'sessions' }
   resources :comments, except: [:new, :edit]
   resources :posts, except: [:new, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
