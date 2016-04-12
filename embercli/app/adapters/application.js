88245270ec3262126f179d5f55228da5d20953a2
Rinold Simon <crisrinold@gmail.com>
2016-04-12 12:10:46 +0530

    set adapter

--- embercli/app/adapters/application.js
+++ embercli/app/adapters/application.js
@@ -1,5 +1,13 @@
 //import DS from 'ember-data';
+// import ActiveModelAdapter from 'active-model-adapter';
+// export default ActiveModelAdapter.extend({
+// 	host: 'http://localhost:3000'
+// });
+
 import ActiveModelAdapter from 'active-model-adapter';
-export default ActiveModelAdapter.extend({
-	host: 'http://localhost:3000'
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+
+export default ActiveModelAdapter.extend(DataAdapterMixin, {
+  host: 'http://localhost:3000',
+  authorizer: 'authorizer:devise'
 });
